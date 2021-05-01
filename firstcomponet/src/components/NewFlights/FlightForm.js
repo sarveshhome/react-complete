434e23f8e89ae3a0c2cdeffdec36d502ca745898
sarveshhome <sarvesh.carlton@gmail.com>
2021-05-01 18:44:44 +0100

    Adding Form Inputs

---
+++ firstcomponet/src/components/NewFlights/FlightForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './FlightForm.css';
+const FlightForm = () => {
+    return (
+        <form>
+            <div className="new-flight__controls">
+                <div className="new-flight__control">
+                    <label>Title</label>
+                    <input type="text"></input>
+                </div>
+                <div className="new-flight__control">
+                    <label>Amount</label>
+                    <input type="number" min="0.01" step="0.01"></input>
+                </div>
+                <div className="new-flight__control">
+                    <label>Date</label>
+                    <input type="date" min="2019-01-01" max="2023-12-30"></input>
+                </div>
+            </div>
+            <div className="new-flight__action">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    );
+}
+
+export default FlightForm;
