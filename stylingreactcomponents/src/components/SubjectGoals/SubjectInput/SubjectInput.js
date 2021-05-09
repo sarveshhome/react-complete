32d0ad43489d68366a489f216c4610e867d1baa0
sarveshhome <sarvesh.carlton@gmail.com>
2021-05-09 18:27:00 +0100

    Dynamic inline Styples

--- stylingreactcomponents/src/components/SubjectGoals/SubjectInput/SubjectInput.js
+++ stylingreactcomponents/src/components/SubjectGoals/SubjectInput/SubjectInput.js
@@ -5,14 +5,20 @@ import './SubjectInput.css';
 
 const SubjectInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  //set true false for color
+  const [isValid,setIsValid]= useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length >0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
     if(enteredValue.trim().length===0){
+      setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
@@ -21,8 +27,13 @@ const SubjectInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Subject Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isValid ? 'red':'black'}}>Subject Goal</label>
+        <input 
+        style={{
+          borderColor: !isValid ? 'red':'black',
+          background:  !isValid ? '#bbbbbb':'transparent'
+        }} 
+        type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
