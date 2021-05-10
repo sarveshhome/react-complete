fd2df257ccf8b761a658777e1cf5a262906cba6e
sarveshhome <sarvesh.carlton@gmail.com>
2021-05-10 19:30:11 +0100

    Style components in subject Input

--- stylingreactcomponents/src/components/SubjectGoals/SubjectInput/SubjectInput.js
+++ stylingreactcomponents/src/components/SubjectGoals/SubjectInput/SubjectInput.js
@@ -1,7 +1,42 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './SubjectInput.css';
+
+const FormControl = styled.div`
+
+  margin: 0.5rem 0;
+
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #fad0ec;
+  border-color: #8b005d;
+}
+&.invalid input{
+  border-color: red;
+  background: rgb(248, 186, 186);
+}
+&.invalid label{
+  color: red;
+}
+`;
 
 const SubjectInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -26,11 +61,11 @@ const SubjectInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid':''}`}>
+      <FormControl className={!isValid && 'invalid'}>
         <label >Subject Goal</label>
         <input         
         type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
